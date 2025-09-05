3f7d4c00d9efe536a49aef517d427399c4f8651f
Stefanus Titan Elianto <107969099+StefanusTitan@users.noreply.github.com>
2025-09-05 20:25:51 +0700

    feat: add header with dashboard title and icon to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { getAllUnits } from "@/apis/units";
 import UnitList from "@/components/UnitList";
+import { LayoutDashboard } from "lucide-react";
 
 export default async function Home() {
   const data = await getAllUnits();
-  
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold">
+          <LayoutDashboard className="inline-block mr-2" size={56} />
+          Unit Management Dashboard
+        </h1>
         <UnitList units={data.units} />
       </main>
     </div>
